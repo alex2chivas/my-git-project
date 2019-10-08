ebb5abd1ff73bd6a53c1d817db6d69bc20758802
Alexis Flores <alex2chivas@hotmail.com>
2019-10-07 20:27:22 -0700

    Initial commit

---
+++ my_file.py
@@ -0,0 +1,23 @@
+print ('Hi Git')
+
+
+
+'''
+-START-
+git init
+starts a new repository in this directory
+----------
+-Show Hiding Git Diretory-
+ls -la
+-----------
+- Shows status of repository-
+git status
+-----------
+-Adding Files-
+git add . 
+this will add everything
+git add my_file.py
+this adds specific file
+------------
+--
+'''
