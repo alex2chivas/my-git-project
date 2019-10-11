5dbb3cf28ceeff70a433f0f90cd94704b4c4b4a1
Alexis Flores <alex2chivas@hotmail.com>
2019-10-11 10:09:02 -0700

    multiple changes

---
+++ git_notes.py
@@ -0,0 +1,46 @@
+'''
+removing git
+setting = **/.git
+----------
+- If you get error status after you used .gitignore file-
+git rm -r --cached .
+removes all local cache changes 
+'''
+'''
+-START-
+git init
+starts a new repository in this directory
+----------
+-Show Hiding Git Diretory-
+ls -la
+-----------
+- Shows status of repository-
+git status
+-----------
+-Adding Files-
+git add . 
+this will add everything
+git add my_file.py
+this adds specific file
+------------
+-To Commit the file-
+git commit -m "add message"
+-------------
+-Creat repository in Github-
+ex : git remote add origin https://github.com/alex2chivas/my-git-project
+.git
+-check remotes 
+git remote -v
+-------------
+-Push items-
+ex : git push -u origin master
+-------------
+-Pull items-
+git pull
+--------------
+- Viewing Branches-
+git branch
+----------------
+-Switching and creating New Branch-
+git checkout -b readme-styling
+'''
